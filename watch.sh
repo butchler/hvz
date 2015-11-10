b425f46237f23beff9dc6533d7a83ddb444347c3
Adam Buechler <butchler@gmail.com>
2015-11-10 13:31:39 -0500

    Add prefixes to watch.sh output.

--- watch.sh
+++ watch.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # Automatically bundle code when files change.
-watchify src/index.js -o bundle.js -d -v &
+# -d = generate source maps, -v = verbose.
+watchify src/index.js -o bundle.js -d -v 2>&1 | sed -e 's/^/WATCHIFY /' &
 
 # Start livereload server, accessed by script in index.html.
-livereload . -p 8001 -d &
+# -d = verbose
+# -x node_modules = don't watch files in the node_modules folder.
+livereload ./bundle.js -p 8001 -d -x node_modules 2>&1 | sed -e 's/^/LIVERELOAD /' &
 
 # Start local HTTP server.
-python -m http.server
+python -m http.server 2>&1 | sed -e 's/^/SERVER /'
