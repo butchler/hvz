76853ceea382f3c53d5b8a5f6953108f200947f4
Adam Buechler <butchler@gmail.com>
2015-11-14 10:27:13 -0500

    Move HTML/CSS/outputted JavaScript to dist/ folder.

--- watch.sh
+++ watch.sh
@@ -2,12 +2,12 @@
 
 # Automatically bundle code when files change.
 # -d = generate source maps, -v = verbose.
-watchify src/index.js -o bundle.js -d -v 2>&1 | sed -e 's/^/WATCHIFY /' &
+watchify src/client.js -o ./dist/bundle.js -d -v 2>&1 | sed -e 's/^/WATCHIFY /' &
 
 # Start livereload server, accessed by script in index.html.
 # -d = verbose
 # -x node_modules = don't watch files in the node_modules folder.
-livereload ./bundle.js -p 8001 -d -x node_modules 2>&1 | sed -e 's/^/LIVERELOAD /' &
+livereload ./dist/client.js -p 8001 -d -x node_modules 2>&1 | sed -e 's/^/LIVERELOAD /' &
 
 # Start local HTTP server.
-python -m http.server 2>&1 | sed -e 's/^/SERVER /'
+cd ./dist && python -m http.server 2>&1 | sed -e 's/^/SERVER /'
